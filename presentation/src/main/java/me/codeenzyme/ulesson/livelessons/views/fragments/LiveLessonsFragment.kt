2ff5381b2f5d9cbbbb71e86a245f3b678152ea4f
Victor-El <victorelezua@gmail.com>
2021-09-17 04:03:05 +0100

    add navigation animation and populate live lessons spinner

--- presentation/src/main/java/me/codeenzyme/ulesson/livelessons/views/fragments/LiveLessonsFragment.kt
+++ presentation/src/main/java/me/codeenzyme/ulesson/livelessons/views/fragments/LiveLessonsFragment.kt
@@ -2,8 +2,10 @@ package me.codeenzyme.ulesson.livelessons.views.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import me.codeenzyme.ulesson.R
 import me.codeenzyme.ulesson.databinding.FragmentLiveLessonsBinding
@@ -15,9 +17,26 @@ class LiveLessonsFragment: Fragment(R.layout.fragment_live_lessons) {
     private val viewModel by viewModels<LiveLessonsViewModel>()
     private var viewBinding: FragmentLiveLessonsBinding? = null
 
+    private lateinit var subjectsArrayAdapter: ArrayAdapter<String>
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewBinding = FragmentLiveLessonsBinding.bind(view)
+
+        subjectsArrayAdapter =
+            ArrayAdapter(requireContext(), R.layout.layout_spinner_item, viewModel.subjects)
+        subjectsArrayAdapter.setDropDownViewResource(R.layout.item_layout_spinner_dropdown)
+
+        viewBinding?.let {
+            it.subjectSpinner.adapter = subjectsArrayAdapter
+
+            it.backButton.setOnClickListener {
+                findNavController().navigateUp()
+            }
+            it.fab.setOnClickListener {
+                findNavController().navigate(LiveLessonsFragmentDirections.actionLiveLessonsFragmentToMyLessonsFragment())
+            }
+        }
     }
 
     override fun onDestroyView() {
--- presentation/src/main/java/me/codeenzyme/ulesson/mylessons/views/fragments/MyLessonsFragment.kt
+++ presentation/src/main/java/me/codeenzyme/ulesson/mylessons/views/fragments/MyLessonsFragment.kt
@@ -5,13 +5,14 @@ import android.view.View
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import me.codeenzyme.ulesson.R
 import me.codeenzyme.ulesson.databinding.FragmentMyLessonsBinding
 import me.codeenzyme.ulesson.mylessons.viewmodels.MyLessonsViewModel
 
 @AndroidEntryPoint
-class MyLessonsFragment: Fragment(R.layout.fragment_my_lessons) {
+class MyLessonsFragment : Fragment(R.layout.fragment_my_lessons) {
 
     private var viewBinding: FragmentMyLessonsBinding? = null
 
@@ -22,11 +23,21 @@ class MyLessonsFragment: Fragment(R.layout.fragment_my_lessons) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewBinding = FragmentMyLessonsBinding.bind(view)
-        subjectsArrayAdapter = ArrayAdapter(requireContext(), R.layout.layout_spinner_item, viewModel.subjects)
+        subjectsArrayAdapter =
+            ArrayAdapter(requireContext(), R.layout.layout_spinner_item, viewModel.subjects)
         subjectsArrayAdapter.setDropDownViewResource(R.layout.item_layout_spinner_dropdown)
         viewBinding?.let {
             it.subjectSpinner.adapter = subjectsArrayAdapter
             it.addLiveLessons.clipToOutline = true
+
+            it.addLiveLessons.setOnClickListener {
+                findNavController().navigate(MyLessonsFragmentDirections
+                    .actionMyLessonsFragmentToLiveLessonsFragment())
+            }
+
+            it.backButton.setOnClickListener {
+                findNavController().navigateUp()
+            }
         }
     }
 
